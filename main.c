93de8e508310faa82a48d6b4ceaffb2e9bb09541
tangchunling230 <tangchunling@druid.tech>
2026-07-07 15:10:51 +0800

    初始化项目：添加 main.c 文件夹

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
-int main(){
-    printf("hello world,it me,hello hello\n");
-    printf("hello world,it me,hello hello\n");
-    printf("hello world,it me,hello hello\n");
+int main()
+{
+    printf("系统启动...\n");
     return 0;
 }
